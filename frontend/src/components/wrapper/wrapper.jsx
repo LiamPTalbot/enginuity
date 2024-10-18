ab58d6e69dad37b3638443f9f8a0e2169320099d
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-18 09:55:40 +0100

    end of morning session

---
+++ frontend/src/components/wrapper/wrapper.jsx
@@ -0,0 +1,9 @@
+import 'enginuity/frontend/src/pages/dashboard.css'
+
+export function Wrapper ({ children }) {
+    return (
+        <div className='wrapper'>
+            {children}
+        </div>        
+    )
+}
