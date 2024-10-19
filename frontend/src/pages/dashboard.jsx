81ad279f4ebaef7ad6fc15a243bd01ac652afbe1
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-19 13:00:35 +0100

    finished saturday seshwari naan

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,18 +1,13 @@
 import { Wrapper } from '../components/wrapper/wrapper.jsx';
 import { Nav } from '../components/wrapper/nav/nav.jsx';
 import { Body } from '../components/wrapper/body/body.jsx';
-import { Sidebar } from '../components/wrapper/body/sidebar/sidebar.jsx';
-import { Table } from '../components/wrapper/body/table/table.jsx';
 import { Footer } from '../components/wrapper/footer/footer.jsx';
 
 export function Dashboard() {
     return (
         <Wrapper>
             <Nav />
-            <Body>
-                <Sidebar />
-                <Table />
-            </Body>
+            <Body />
             <Footer />
         </Wrapper>
     );
