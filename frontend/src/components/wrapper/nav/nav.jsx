ff67d769cdecd1234dbe4bc5ecee9180fad04a05
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-20 12:08:36 +0100

    sorted titles

--- frontend/src/components/wrapper/nav/nav.jsx
+++ frontend/src/components/wrapper/nav/nav.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faCircleQuestion, faMagnifyingGlass, faQuestion, faUser } from "@fortawesome/free-solid-svg-icons";
+
 export function Nav() {
     return (
         <div className='nav'>
-            <h1>Nav Component</h1>
+            <div className="nav-titles">
+                <h1>engine Ai</h1>
+                <h4>- using Machine Learning to provide predictive capabilities and an intelligent supply chain</h4>
+            </div>
+            <div className="nav-logos">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                <FontAwesomeIcon icon={faCircleQuestion} />
+                <FontAwesomeIcon icon={faBell} />
+                <FontAwesomeIcon icon={faUser} />
+            </div>
         </div>
     )
 }
