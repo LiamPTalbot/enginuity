50cda88bfe102e69cb382eefe3fae34dc1fa87da
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-25 09:29:05 +0100

    finished morning seshwari

--- frontend/src/components/wrapper/body/table/table.jsx
+++ frontend/src/components/wrapper/body/table/table.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import { FailureReport } from "./failure_report";
 import { FuelReport } from "./fuel_report";
+import { DataFrame } from "./data_frame";
 import '@royalnavy/fonts';
 import { TabSet, TabSetItem } from '@royalnavy/react-component-library'; 
 
 export function Table() {
     return (
-        <div className='table'>
-            <div className='tabNav'>
-                <TabSet isFullWidth>
+        <div className='tabNav'>
+                <TabSet isFullWidth className="tabSetContainer">
                     <TabSetItem title="Failure Report Input">
                         <FailureReport/>
                     </TabSetItem>
                     <TabSetItem title="Fuel Report Input">
                         <FuelReport/>
                     </TabSetItem>
+                    <TabSetItem title="DataFrame">
+                        <DataFrame/>
+                    </TabSetItem>
                     <TabSetItem title="Predictive Analytics">
                         <p>
-                            Example tab 2 content
+                            Example tab 4 content
                         </p>
                     </TabSetItem>
                 </TabSet>
-            </div>
         </div>
     );
 }
