aac5681df56c7125acd267f2a784b802c4704db4
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-30 16:52:31 +0000

    tweaked Props

--- frontend/src/components/wrapper/body/body.jsx
+++ frontend/src/components/wrapper/body/body.jsx
@@ -2,7 +2,7 @@ import { Sidebar } from "./sidebar/sidebar"
 import { Table } from "./table/table"
 
 export function Body() {
-    // passing the sidebar Title as a prop to from body (parent) to sidebar (child)
+    // passing the sidebar Title as a prop from body (parent) to sidebar (child)
     const sidebarTitle = "Dynamic Sidebar Title"
     return (
         <div className='body'>
